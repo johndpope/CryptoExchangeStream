1b4d2656ba9e4bca2a54678074eacac3204c1cb6
Joe Blau <josephblau@gmail.com>
2017-09-03 20:05:52 -0400

    Initial Commit

---
+++ CryptoExchangeStream/CryptoExchangeStream.h
@@ -0,0 +1,19 @@
+//
+//  CryptoExchangeStream.h
+//  CryptoExchangeStream
+//
+//  Created by Joseph Blau on 9/3/17.
+//  Copyright © 2017 Joseph Blau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CryptoExchangeStream.
+FOUNDATION_EXPORT double CryptoExchangeStreamVersionNumber;
+
+//! Project version string for CryptoExchangeStream.
+FOUNDATION_EXPORT const unsigned char CryptoExchangeStreamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CryptoExchangeStream/PublicHeader.h>
+
+
